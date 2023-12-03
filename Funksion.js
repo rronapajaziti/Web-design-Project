c825350187713123924c8907c58350cf073b4349
rronapajaziti <rrona2004@gmail.com>
2023-12-03 20:11:12 +0100

    hotels dhe contact

--- Funksion.js
+++ Funksion.js
@@ -1,33 +1,48 @@
-const slides = document.querySelector(".slides")
-var i =0;
-var imgVarg = [
-    "Photos/hotel1.jpg",
-    "Photos/hotel2.jpg",
-    "Photos/hotel4.jpg",
-    "Photos/hotel5.jpg"
-]
-function nextSlide(){
-    document.getElementById('slide').src = imgVarg[i];
-    if(i< imgVarg.length -1){
-        i++;
+// const body = document.body;
+const slider = document.querySelectorAll('.slide');
+const prev = document.querySelectorAll('.previous');
+const next = document.querySelectorAll('.next');
+
+let i = 0;
+
+next[0].addEventListener('click',() => {
+    i++;
+    if(i >slider.length -1){
+        i =0;
     }
-    else{
-        i=0;
+    setBgToBody();
+    setActiveSlides();
+});
+prev[0].addEventListener('click',() => {
+    i--;
+    if(i < 0 ){
+        i = slider.length -1;
     }
-    document.body.addEventListener('load',nextSlide());
-    
+    setBgToBody();
+    setActiveSlides();
+});
+
+function setBgToBody(){
+    body.style.backgroundImage = slider[i].style.backgroundImage
 }
-function prevSlide(){
-    document.getElementById('slide').src = imgVarg[i];
-    if(i>0){
-        i--;
-    }else{
-        i= imgVarg.length-1;
+function setActiveSlides(){
+    slider.forEach((slide) => slide.classList.remove('active'));
+    slides[i].classList.add('active');
+}
+
+
+
+function emalValid(){
+
+    var email =document.getElementById('email').value;
+    var emailRegex = /^[^\s@] + @[^\s@] + \.[^\s@] +$/;
+    if(!emailRegex.test(email)){
+        alert('Please enter a valid email address');
+        return false;
+
     }
-    document.body.addEventListener('load',prevSlide());
 
 }
-
 
 // -------------------------------------Log in-----------------------------------
 
