dc1f1bdc7b4c0bbcd2693cecd208f4274454d289
rronapajaziti <rrona2004@gmail.com>
2023-12-05 11:49:35 +0100

    responsive

--- Funksion.js
+++ Funksion.js
@@ -1,35 +1,20 @@
-// const body = document.body;
-const slider = document.querySelectorAll('.slide');
-const prev = document.querySelectorAll('.previous');
-const next = document.querySelectorAll('.next');
+var slideIndex = 1;
+showDivs(slideIndex);
 
-let i = 0;
-
-next[0].addEventListener('click',() => {
-    i++;
-    if(i >slider.length -1){
-        i =0;
-    }
-    setBgToBody();
-    setActiveSlides();
-});
-prev[0].addEventListener('click',() => {
-    i--;
-    if(i < 0 ){
-        i = slider.length -1;
-    }
-    setBgToBody();
-    setActiveSlides();
-});
-
-function setBgToBody(){
-    body.style.backgroundImage = slider[i].style.backgroundImage
-}
-function setActiveSlides(){
-    slider.forEach((slide) => slide.classList.remove('active'));
-    slides[i].classList.add('active');
+function plusDivs(n) {
+  showDivs(slideIndex += n);
 }
 
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
 
 
 function emalValid(){
