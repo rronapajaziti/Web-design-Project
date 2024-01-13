e2e173aa0e13ae07fa92eb6436661227b2b78507
erdinabislimi <149427167+erdinabislimi@users.noreply.github.com>
2024-01-13 15:45:24 +0100

    Krijimi i nje page te ri "Offers.php"

---
+++ Offers.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style.css">
+    <script src="https://kit.fontawesome.com/0feed4ec37.js" crossorigin="anonymous"></script>
+    <title>Oferta</title>
+</head>
+<body>
+<section class="destinacionet">
+        <h1>Offers</h1>
+        <div class="container">
+            <div class="row">
+                <div class="column">
+                    <div class="box">
+                        <div class="images">
+                            <img src="Photos/Tajland.jpeg" alt="Thailand" class="img">
+                            <div class="cmimi">
+                                <p>299€</p>
+                            </div>
+                            <div class="rating">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star-half"></i>
+                            </div>
+                        </div>
+                        <div class="detaje">
+                            <h4>Thailand</h4>
+                            <p>Rambling mountains and lumbering elephants to its far-flung islands, 
+                                heavenly beaches and kaleidoscopic marine life,
+                                 cloud-piercing skyscrapers and honking traffic,
+                                 Thailand will impress you at every turn</p>
+                                <div class="icona">
+                                <p><i class="fa fa-location-dot"></i>Thailand</p>
+                                <p><i class="fa fa-sun"></i>4 Days</p>
+                                <p><i class="fa fa-moon"></i>3 Nights</p>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+          
+                <div class="column">
+                    <div class="box">
+                        <div class="images">
+                            <img src="Photos/NewYork.jpeg" alt="NewYork" class="img">
+                            <div class="cmimi">
+                                <p>150€</p>
+                            </div>
+                            <div class="rating">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star-half"></i>
+                            </div>
+                        </div>
+                        <div class="detaje">
+                            <h4>New York</h4>
+                            <p>It is one of the things that make New York City so unique because its features are simply incredible. 
+                                It has beautiful wildflowers, green spaces, stunning architecture, and outdoor art installations.Briliant!.</p>
+                                    <div class="icona">
+                                        <p><i class="fa fa-location-dot"></i>NewYork</p>
+                                        <p><i class="fa fa-sun"></i>2 Days</p>
+                                        <p><i class="fa fa-moon"></i>3 Nights</p>
+                                    </div>
+                        </div>       
+                    </div>
+                </div>
+
+                    <div class="column">
+                       <div class="box">
+                        <div class="images">
+                            <img src="Photos/monaco.jpeg" alt="monaco" class="img">
+                            <div class="cmimi">
+                                <p>200€</p>
+                            </div>
+                            <div class="rating">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                            </div>
+                        </div>
+                        <div class="detaje">
+                            <h4>Monaco</h4>
+                            <p>Paradise awaits you… If your idea of paradise is a pristine tropical island with swaying palm trees and pure white beaches surrounded by crystalline lagoons of varying shades of 
+                                blue – then the Monaco will definitely fit the bill!</p>
+                                 <div class="icona">
+                                    <p><i class="fa fa-location-dot"></i>Monaco</p>
+                                    <p><i class="fa fa-sun"></i>3 Days</p>
+                                    <p><i class="fa fa-moon"></i>2 Nights</p>
+                                </div>
+                </div>
+                
+
+            </div>
+        </div>
+        <div class="column">
+                       <div class="box">
+                        <div class="images">
+                            <img src="Photos/Los Angeles.jpeg" alt="Los Angeles" class="img">
+                            <div class="cmimi">
+                                <p>230€</p>
+                            </div>
+                            <div class="rating">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                            </div>
+                        </div>
+                        <div class="detaje">
+                            <h4>Los Angeles</h4>
+                            <p>The huge, sprawling, and tortuously shaped city of Los Angeles occupies a sizable portion of the southern part of the county. It too has a varied topography, climbing from sea level at the beach community of Venice to Mount Lukens!</p>
+                                 <div class="icona">
+                                    <p><i class="fa fa-location-dot"></i>Los Angeles</p>
+                                    <p><i class="fa fa-sun"></i>2 Days</p>
+                                    <p><i class="fa fa-moon"></i>1 Nights</p>
+                                </div>
+                </div>
+                
+
+            </div>
+        </div>
+        <div class="column">
+                       <div class="box">
+                        <div class="images">
+                            <img src="Photos/Indonesia.jpeg" alt="Indonesia" class="img">
+                            <div class="cmimi">
+                                <p>400€</p>
+                            </div>
+                            <div class="rating">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                            </div>
+                        </div>
+                        <div class="detaje">
+                            <h4>Indonesia</h4>
+                            <p>home to some of nature's most beautiful natures, including beaches and deep seas, as well as tropical forests and mountains. Be wowed by gorgeous beaches such as those in Bali, Lombok, Nusa Dua, and the tranquil Gili Islands.</p>
+                                 <div class="icona">
+                                    <p><i class="fa fa-location-dot"></i>Indonesia</p>
+                                    <p><i class="fa fa-sun"></i>3 Days</p>
+                                    <p><i class="fa fa-moon"></i>2 Nights</p>
+                                </div>
+                </div>
+                
+
+            </div>
+        </div>
+        <div class="column">
+                       <div class="box">
+                        <div class="images">
+                            <img src="Photos/croatia.jpeg" alt="croatia" class="img">
+                            <div class="cmimi">
+                                <p>280€</p>
+                            </div>
+                            <div class="rating">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                            </div>
+                        </div>
+                        <div class="detaje">
+                            <h4>Croatia</h4>
+                            <p>A beautiful country with over four million smiling faces and more than a thousand islands, where the sea literally plays music, the sun waves goodbye as it sets down for a nap, and enchanting stories for every occasion.</p>
+                                 <div class="icona">
+                                    <p><i class="fa fa-location-dot"></i>Croatia</p>
+                                    <p><i class="fa fa-sun"></i>3 Days</p>
+                                    <p><i class="fa fa-moon"></i>2 Nights</p>
+                                </div>
+                </div>
+                
+
+            </div>
+        </div>
+        <div class="column">
+                       <div class="box">
+                        <div class="images">
+                            <img src="Photos/dubai.jpeg" alt="dubai" class="img">
+                            <div class="cmimi">
+                                <p>170€</p>
+                            </div>
+                            <div class="rating">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                            </div>
+                        </div>
+                        <div class="detaje">
+                            <h4>Dubai</h4>
+                            <p>The City of Gold is known for its amazing architecture, breath-taking landmarks, the magnificent shopping festival (of course) and the warmth of its people.
+                            Dubai has just about everything you would need for a perfect holiday trip.
+                            </p>
+                                 <div class="icona">
+                                    <p><i class="fa fa-location-dot"></i>Dubai</p>
+                                    <p><i class="fa fa-sun"></i>3 Days</p>
+                                    <p><i class="fa fa-moon"></i>2 Nights</p>
+                                </div>
+                </div>
+                
+
+            </div>
+        </div>
+        <div class="column">
+                       <div class="box">
+                        <div class="images">
+                            <img src="Photos/tokyo.jpeg" alt="tokyo" class="img">
+                            <div class="cmimi">
+                                <p>370€</p>
+                            </div>
+                            <div class="rating">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                            </div>
+                        </div>
+                        <div class="detaje">
+                            <h4>Tokyo</h4>
+                            <p>Tokyo is a foodie paradise with more Michelin-starred restaurants than any other city in the world, along with hundreds of cheap and delicious ramen shops.
+                            Tokyo has mild spring weather, followed by hot and humid summers punctuated with typhoons.
+                            </p>
+                                 <div class="icona">
+                                    <p><i class="fa fa-location-dot"></i>Tokyo</p>
+                                    <p><i class="fa fa-sun"></i>3 Days</p>
+                                    <p><i class="fa fa-moon"></i>2 Nights</p>
+                                </div>
+                </div>
+                
+
+            </div>
+        </div>
+        <div class="column">
+                       <div class="box">
+                        <div class="images">
+                            <img src="Photos/turkey.jpeg" alt="turkey" class="img">
+                            <div class="cmimi">
+                                <p>250€</p>
+                            </div>
+                            <div class="rating">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                            </div>
+                        </div>
+                        <div class="detaje">
+                            <h4>Turkey</h4>
+                            <p>Tourism in Turkey is focused largely on a variety of historical sites, and on seaside resorts along its Aegean and Mediterranean Sea coasts
+                            the iconic Hagia Sophia in Istanbul, and the otherworldly landscapes of Cappadocia
+                            </p>
+                                 <div class="icona">
+                                    <p><i class="fa fa-location-dot"></i>Turkey</p>
+                                    <p><i class="fa fa-sun"></i>3 Days</p>
+                                    <p><i class="fa fa-moon"></i>4 Nights</p>
+                                </div>
+                </div>
+                
+
+            </div>
+        </div>
+    </section>
+    
+</body>
+</html>
